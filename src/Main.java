241e74d9878868880b0ba425ee68dc45a59a32c6
Evgeniy-Zarubin <Uncontrollable69@gmail.com>
2024-11-09 11:14:17 +1000

    Task #5 added var frog

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,14 @@
 public class Main {
     public static void main(String[] args) {
-        var dog = 0.8;
+       /* var dog = 0.8;
         var cat = 3.6;
         var paper = 763789;
         var friend = 19;
-        System.out.println(friend);
-        System.out.println(friend+2);
-        System.out.println(friend/7);
+        */
+        var frog = 3.5;
+        System.out.println(frog);
+        System.out.println(frog+10);
+        System.out.println(frog/3.5);
+        System.out.println((frog/3.5)+4);
     }
 }
