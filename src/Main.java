4100ca49798595ca246a64e824654c2b158fdcd1
Evgeniy-Zarubin <Uncontrollable69@gmail.com>
2024-11-09 11:30:34 +1000

    Task #8 Employers in Company

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,6 @@ public class Main {
     public static void main(String[] args) {
         var allTime = 640;
         var workDay = 8;
-        System.out.println("Всего работников в компании - " + allTime/workDay);
+        System.out.println("Если в компании работает " + ((allTime/workDay)+94) + " человек то всего " + ((allTime/workDay)+94)*8 + " часов");
     }
 }
