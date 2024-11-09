82a979c64f70eb6742e6745d0461629622cadaa8
Evgeniy-Zarubin <Uncontrollable69@gmail.com>
2024-11-09 11:17:37 +1000

    Task #6 Two Fighters

--- src/Main.java
+++ src/Main.java
@@ -1,14 +1,8 @@
 public class Main {
     public static void main(String[] args) {
-       /* var dog = 0.8;
-        var cat = 3.6;
-        var paper = 763789;
-        var friend = 19;
-        */
-        var frog = 3.5;
-        System.out.println(frog);
-        System.out.println(frog+10);
-        System.out.println(frog/3.5);
-        System.out.println((frog/3.5)+4);
+        var Fighter1 = 78.2;
+        var Fighter2 = 82.7;
+        System.out.println(Fighter1+Fighter2);
+        System.out.println(Fighter1-Fighter2);
     }
 }
