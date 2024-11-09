51c27ba14509f2a5bf443631fbc96aa70a1647ee
Evgeniy-Zarubin <Uncontrollable69@gmail.com>
2024-11-09 11:20:23 +1000

    Task #7 Remainder of division

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,6 @@ public class Main {
     public static void main(String[] args) {
         var Fighter1 = 78.2;
         var Fighter2 = 82.7;
-        System.out.println(Fighter1+Fighter2);
-        System.out.println(Fighter1-Fighter2);
+        System.out.println(Fighter2 % Fighter1);
     }
 }
