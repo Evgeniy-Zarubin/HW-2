b0b19ae6999747a6efcd53675ac4c7272c62bfae
Evgeniy-Zarubin <Uncontrollable69@gmail.com>
2024-11-09 10:57:04 +1000

    Add +4 on all var New Version

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,10 @@
 public class Main {
     public static void main(String[] args) {
-        var dog = 4.8;
-        var cat = 7.6;
-        var paper = 763793;
-        System.out.println(dog);
-        System.out.println(cat);
-        System.out.println(paper);
+        var dog = 0.8;
+        var cat = 3.6;
+        var paper = 763789;
+        System.out.println(dog+4);
+        System.out.println(cat+4);
+        System.out.println(paper+4);
     }
 }
