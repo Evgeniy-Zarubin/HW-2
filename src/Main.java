481f7bec8a9590a0222eb51db6f946edff20fada
Evgeniy-Zarubin <Uncontrollable69@gmail.com>
2024-11-09 11:09:41 +1000

    Task #4 added var friend

--- src/Main.java
+++ src/Main.java
@@ -3,8 +3,9 @@ public class Main {
         var dog = 0.8;
         var cat = 3.6;
         var paper = 763789;
-        System.out.println(dog-3.5);
-        System.out.println(cat-1.6);
-        System.out.println(paper-7639);
+        var friend = 19;
+        System.out.println(friend);
+        System.out.println(friend+2);
+        System.out.println(friend/7);
     }
 }
